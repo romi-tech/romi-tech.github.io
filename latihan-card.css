5a1a438cf994f3d3da79b4cdc88c06741ca95bdd
romi-tech <romidwiputrasnk12@gmail.com>
2021-06-03 07:56:44 +0700

    first commit

---
+++ latihan-card.css
@@ -0,0 +1,21 @@
+*{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+}
+body{
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.card-pricing.popular {
+    z-index: 1;
+    border: 3px solid #007bff;
+}
+.card-pricing .list-unstyled li {
+    padding: .5rem 0;
+    color: #6c757d;
+}
+
+  
